935a57f21770543fb7ef5630d97a5e1ecb964526
Thomas Antony S <srtomsam@gmail.com>
2026-01-29 17:23:15 +0530

    reduced toast-time & remove search when no article

--- frontend/src/components/Toast.jsx
+++ frontend/src/components/Toast.jsx
@@ -5,7 +5,7 @@ const Toast = ({ message, type, onClose }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       onClose();
-    }, 5000);
+    }, 3000);
     return () => clearTimeout(timer);
   }, [onClose]);
 
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -207,31 +207,35 @@ const Profile = () => {
               <div ref={libraryTopRef} className="animate-in fade-in slide-in-from-bottom-2 duration-300 pb-20">
                 <h3 className="text-4xl md:text-6xl font-semibold uppercase tracking-tighter mb-10">Library</h3>
 
-              <div className="flex items-center gap-4 mb-10 w-full md:w-fit">
-                <div className="flex items-center p-2 border-2 border-black bg-white focus-within:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all">
-                  <input 
-                    type="text" 
-                    placeholder="SEARCH BY NEWS TITLE." 
-                    value={searchQuery}
-                    onChange={(e) => {
-                      setSearchQuery(e.target.value);
-                      setCurrentPage(1); 
-                    }}
-                    className="w-full md:w-64 pr-4 text-[14px] font-black tracking-wide placeholder:text-black focus:outline-none bg-transparent"
-                  />
-                  <div className="shrink-0 border-l-2 border-black pl-2 hover:cursor-pointer">
-                    <Search size={18} strokeWidth={3} />
+              {savedArticles.length > 0 ?
+                <div className="flex items-center gap-4 mb-10 w-full md:w-fit">
+                  <div className="flex items-center p-2 border-2 border-black bg-white focus-within:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all">
+                    <input 
+                      type="text" 
+                      placeholder="SEARCH BY NEWS TITLE." 
+                      value={searchQuery}
+                      onChange={(e) => {
+                        setSearchQuery(e.target.value);
+                        setCurrentPage(1); 
+                      }}
+                      className="w-full md:w-64 pr-4 text-[14px] font-black tracking-wide placeholder:text-black focus:outline-none bg-transparent"
+                    />
+                    <div className="shrink-0 border-l-2 border-black pl-2 hover:cursor-pointer">
+                      <Search size={18} strokeWidth={3} />
+                    </div>
                   </div>
+                    
+                  {searchQuery.trim() !== "" && (
+                    <button onClick={handleClear}
+                      className="px-4 py-2 border-2 border-black bg-white text-[14px] font-black uppercase tracking-wide hover:bg-black hover:text-white transition-all cursor-pointer"
+                    >
+                      Clear
+                    </button>
+                  )}
                 </div>
-                  
-                {searchQuery.trim() !== "" && (
-                  <button onClick={handleClear}
-                    className="px-4 py-2 border-2 border-black bg-white text-[14px] font-black uppercase tracking-wide hover:bg-black hover:text-white transition-all cursor-pointer"
-                  >
-                    Clear
-                  </button>
-                )}
-              </div>
+                :
+                null
+              }
 
                 <div className="grid gap-4">
                   {loadingSaved ? (
